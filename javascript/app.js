0bf89e621179b3726ccef9f1f2eefc2c33d357a2
Chris <chris@freeperiod.co.uk>
2022-01-31 15:17:06 +0000

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+
+function toggleDescription(){
+
+    let briefCollapse = document.getElementById('briefDescription');
+    let fullCollapse = document.getElementById('fullDescription');
+
+
+    let bsCollapseBrief = new bootstrap.Collapse(briefCollapse);
+    let bsCollapseFull = new bootstrap.Collapse(fullCollapse);
+
+        bsCollapseBrief.toggle();
+        bsCollapseFull.toggle();
+    
+
+}
+
+
+
